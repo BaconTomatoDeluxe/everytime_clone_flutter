fb5f0d04590ef6e480decd35e0cad41d9b7abfc6
qpaqpleqp <qpaqpleqp@korea.ac.kr>
2023-10-10 22:22:40 +0900

    feat: post_preview

--- lib/widgets/listpage/post_preview.dart
+++ lib/widgets/listpage/post_preview.dart
@@ -7,23 +7,24 @@ class PostPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Row(
+      child: Column(
         children: [
-          Column(
+          //게시판명, 글제목, 글 내용 받아와야함
+          Text('자유게시판', style: TextStyle(color: Colors.blueAccent, fontSize: 15, fontWeight: FontWeight.bold),),
+          Text('학년이 높아질 수록', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
+          Text('공부를 열심히하기는커녕 덜하고도 마음만 편해지는듯', style: TextStyle(color: Colors.grey, fontSize: 16)),
+          Row(
             children: [
-              //게시판명, 글제목, 글 내용
-              Text('자유게시판', style: TextStyle(color: Colors.blueAccent, fontSize: 15, fontWeight: FontWeight.bold),),
-              Text('학년이 높아질 수록', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
-              Text('data')
-              Row(
-                children: [
-                  ThumbComment(),
-                  Text('| 07:31 | 익명', style: TextStyle(color: Colors.white.withOpacity(0.3), fontSize: 13),),
-                ],
-              )
+              ThumbComment(),
+              Text('| 07:31 | 익명', style: TextStyle(color: Colors.white.withOpacity(0.3), fontSize: 13),),
             ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10),
+            height: 1,
+            decoration: BoxDecoration(color: Colors.grey.withOpacity(0.3)),
           )
         ],
       ),
